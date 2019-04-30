9b2e6f0fa88287ea40432a985054412cb2869cfe
jackjack <theo.lepine@enseirb-matmeca.fr>
2019-04-30 18:31:59 +0200

    Fix syntaxe HTTPS

--- nip/javascript/server.js
+++ nip/javascript/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var https = require('https');
 var http = require('http');
 var app = require('./app');
+var fs = require('fs');
 
 var key = fs.readFileSync('/srv/certs/privkey.pem');
 var cert = fs.readFileSync('/srv/certs/cert.pem');
